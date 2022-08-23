952bbda6cf55e599faad7a002acb24119e1a7c60
Fabio Macieira <fabiomacieira16@gmail.com>
2022-08-22 23:43:00 -0300

    create teste calculate

---
+++ Test/CalculatorTest.cs
@@ -0,0 +1,27 @@
+public interface ICalculadora
+{
+    int soma(int a, int b);
+}
+
+public class Calculadora : ICalculadora 
+{
+    public int Somar (int a, int b) => a + b;
+}
+
+public class CalculadoraTests
+{
+    [Theory]
+    [inlineData]
+    public void Somar_DoisNumeros_RetornarsomaDosNumeros(int primeiroNumero, int segundoNumero, int resultEsperado)
+    {
+        //Arrange
+        var MockCalculadora = new Mock<ICalculadora>();
+        MockCalculadora.Setup(o => o.Somar(primeiroNumero, segundoNumero)).Returns(resultEsperado);
+
+        //Act
+        var result =  MockCalculadora.Object.Soma(primeiroNumero, segundoNumero);
+
+        //Assert
+        Assert.Equal(result, resultEsperado);
+    }
+}
